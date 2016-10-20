26031e5fa52fbb86d54e48b81dacc818a8543c62
Andrei Dmytruk <mallboroks@mail.ru>
2016-10-21 00:27:37 +0300

    14

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict;'
 $(function () {
-  var $html = $('#test-question').html();
+  var $html = $( '#test-question' ).html();
 
   var questionsAnswers = [
   {
@@ -25,24 +25,53 @@ $(function () {
 }
 ];
 
-localStorage.setItem('answerQuestion', JSON.stringify(questionsAnswers) );
+localStorage.setItem( 'answerQuestion', JSON.stringify(questionsAnswers ) );
 
-var $testBlock = localStorage.getItem('answerQuestion');
+var $testBlock = localStorage.getItem( 'answerQuestion' );
 $testBlock = JSON.parse($testBlock);
 
-  var content = tmpl($html, {
+  var content = tmpl( $html, {
     data: $testBlock
   });
 
-  $('body').append(content);
+  $( 'body' ).append( content );
 
-  var $right = ['Христофор Колумб', '1492', 'Китайцы'],
+  var $right = [ 'Христофор Колумб', '1492', 'Китайцы' ],
   $returnRight = [];
-  console.log($returnRight);
 
-/*var checkbox1 = $('.content-block-1').find('input'),
-checkbox2 = $('.content-block-2').find('input'),
-checkbox3 = $('.content-block-3').find('input');*/
+var $TrueBlockQuestion = $( 'input' );
+
+/*Проверка теста и push правильных вариантов в массив*/
+ $( '#button-check' ).on( 'click', function () {
+   event.preventDefault();
+   for ( var i = 0; i < $TrueBlockQuestion.length; i++ ) {
+     if ( $( $TrueBlockQuestion[i] ).prop( 'checked' ) ) {
+       var valueText = $( $TrueBlockQuestion[i] ).context.value;
+       for ( var j = 0; j < $right.length; j++ ) {
+         if ( valueText === $right[j] ) {
+           $returnRight.push( $right[j] );
+         }
+       }
+     }
+   }
+if ( $right.length === $returnRight.length ) {//сравниваем длину масивов и выводим модальное окно.
+ $( '#overlay' ).fadeIn( 500, function () {
+   $( '#modal_form' )
+   .css( 'display', 'block' )
+   .animate( { opacity: 1, top: '50%' }, 200 );
+   $( '#passed' ).css( 'display', 'block' );
+ });
+} else {
+  $( '#overlay' ).fadeIn(500, function () {
+    $( '#modal_form' )
+    .css( 'display', 'block' )
+    .animate( {opacity: 1, top: '50%'}, 200 );
+    $( '#not_passed' ).css( 'display', 'block' );
+  });
+}
+});
+
+/*Закрываем модальное окно*/
 $('#modal_close, #overlay').click(function () {
   $('#modal_form').animate({opacity: 1, top: '45%'},500,
   function () {
@@ -51,45 +80,4 @@ $('#modal_close, #overlay').click(function () {
   });
 })
 
-var $TrueBlockQuestion = $('input');
-
- $('#button-check').on('click', function () {
-   event.preventDefault();
-   for (var i = 0; i < $TrueBlockQuestion.length; i++) {
-     if ($($TrueBlockQuestion[i]).prop('checked')) {
-       var valueText = $($TrueBlockQuestion[i]).context.value;
-       for (var j = 0; j < $right.length; j++) {
-         if (valueText === $right[j]) {
-           $returnRight.push($right[j]);
-         }
-       }
-     }
-   }
-if ($right.length === $returnRight.length) {
- $('#overlay').fadeIn(500, function () {
-   $('#modal_form')
-   .css('display', 'block')
-   .animate({opacity: 1, top: '50%'},200);
-   $('#passed').css('display', 'block');
- });
-} else {
-  $('#overlay').fadeIn(500, function () {
-    $('#modal_form')
-    .css('display', 'block')
-    .animate({opacity: 1, top: '50%'},200);
-    $('#not_passed').css('display', 'block');
-  });
-}
 });
-});
-
-
-/*for (var i = 0; i < $right.length; i++) {
-  for (var j = 0; j < $returnRight.length; j++) {
-    if ( $right[i] === $returnRight[j]) {
-      alert('Тест пройден))');
-    } else {
-      alert('Тест не пройден))');
-    }
-  }
-}*/
